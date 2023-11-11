402e7be680e8179f9a1a5cfb606e9dfba5733113
Francisco Wallison <franciscowallison@gmail.com>
2023-11-11 08:30:25 -0300

    init carrinho de compra

---
+++ dia_9/compras/src/App.jsx
@@ -0,0 +1,13 @@
+// import { useState } from "react";
+import "./App.css";
+import Header from "./components/Header/Header";
+
+function App() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
